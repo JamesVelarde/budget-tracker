9f1ef08225cd73b595ed73c0f2a89f587bd75c5c
James <jimmyvelarde3302@gmail.com>
2022-02-21 22:57:59 -0700

    Fixed connection issues

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+// const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
 
@@ -16,8 +17,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
   useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
 // routes
